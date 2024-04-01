26199c034abf83055315da7e620b8fecd5698e58
Ansharahkhan76 <anshrahmhirah@gmail.com>
2024-04-01 16:29:40 +0500

    Project of todo list

---
+++ index.ts
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+import chalk  from 'chalk';
+
+let todo_list = [];
+let condition = true;
+
+console.log(chalk.green.bold("\n\t Welcome to my Todo_List project\n"), chalk.blue("\t      ---ANSHARAH KHAN---"));
+
+
+while(condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: "Enter Your New Task : "
+        }
+    ]);
+
+    todo_list.push(addTask.task);
+    console.log(`${addTask.task} Task Added in Todo_list Successfully.`);
+
+    let addMoreTask = await inquirer.prompt([
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more Task?",
+            default : false
+        }
+    ]);
+    condition = addMoreTask.addMore
+};
+
+console.log(`Your Updated Todo_List : ${  todo_list }`)
